a29944527f28588a44a79fd180f73eaf3bbd15bf
mappingmckenna <elise.s.mck@gmail.com>
2016-06-19 14:00:09 +1000

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -2,10 +2,10 @@
 
 // Enter your mapbox map id here to reference it for the base layer,
 // this one references the ugly green map that I made.
-var mapId = 'will-breitkreutz.019h7bl7';
+var mapId = 'el-s.0eg533bn';
 
 // And this is my access token, use yours.
-var accessToken = 'pk.eyJ1Ijoid2lsbC1icmVpdGtyZXV0eiIsImEiOiItMTJGWEF3In0.HEvuRMMVxBVR5-oDYvudxw';
+var accessToken = 'pk.eyJ1IjoiZWwtcyIsImEiOiJjbWxMT3BJIn0.aaOFFPZxMh3dFMaccYwRuw';
 
 // Create the map object with your mapId and token,
 // referencing the DOM element where you want the map to go.
@@ -13,6 +13,6 @@ L.mapbox.accessToken = accessToken;
 var map = L.mapbox.map('map', mapId);
 
 // Set the initial view of the map to the whole US
-map.setView([39, -96], 4);
+//map.setView([39, -96], 4);
 
 // Great, now we have a basic web map!
