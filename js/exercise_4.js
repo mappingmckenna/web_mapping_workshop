6a2e053febd524a1a91240c57ebf8d0024c585b9
mappingmckenna <elise.s.mck@gmail.com>
2016-06-19 17:09:41 +1000

    added popup

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -28,6 +28,12 @@ featureLayer.on('ready', function() {
   map.fitBounds(featureLayer.getBounds());
 });
 
+featureLayer.on('ready', function(){
+  this.eachLayer(function(layer){
+    layer.bindPopup('Welcome to ' + layer.feature.properties.name);
+  });
+});
+
 // Set the initial view of the map to the whole US
 //map.setView([39, -96], 4);
 
