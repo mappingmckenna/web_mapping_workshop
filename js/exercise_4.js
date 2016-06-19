53e623b358747edec8d59f7ceb9e79f5dac8a9c6
mappingmckenna <elise.s.mck@gmail.com>
2016-06-19 17:48:58 +1000

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -28,12 +28,38 @@ featureLayer.on('ready', function() {
   map.fitBounds(featureLayer.getBounds());
 });
 
+//featureLayer.on('ready', function(){
+//  this.eachLayer(function(layer){
+ //   layer.bindPopup('Welcome to ' + layer.feature.properties.name);
+ // });
+//});
+
+var clickHandler = function(e){
+    $('#info').empty();
+  var feature = e.target.feature;
+  $('#sidebar').fadeIn(400, function(){
+var info = '';
+
+    info += '<div>'
+    info += '<h2>' + feature.properties.name + '</h2>'
+    if(feature.properties.phone) info +=   '<p>'  + feature.properties.cuisine + '</p>'
+    if(feature.properties.phone) info +=   '<p>'  + feature.properties.phone + '</p>'
+    if(feature.properties.phone) info +=   '<p>'  + feature.properties.website + '</p>'
+    if(feature.properties.phone) info +=   '<p><a href="' + feature.properties.website + '">'  + feature.properties.website + '</a></p>'
+    info += '</div>'
+
+    $('#info').append(info);
+  });
+};
 featureLayer.on('ready', function(){
   this.eachLayer(function(layer){
-    layer.bindPopup('Welcome to ' + layer.feature.properties.name);
+    layer.on('click', clickHandler);
   });
 });
-
+map.on('click',function(e){
+    $('#info').fadeOut(200);
+    $('#info').empty();
+});
 // Set the initial view of the map to the whole US
 //map.setView([39, -96], 4);
 
